79cd99b8e842781e345f7dd6592544a9f5ad0440
Aleksandra Zhukova <a.hahina@gmail.com>
2020-11-08 13:50:01 +0300

    Camel

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main (String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        int oneYearMonthlyPayment = service.calculate(1_000_000, 1);
+        int oneYearMonthlyPayment = service.calculate(1_000_000, 1, 9.99);
         System.out.println(oneYearMonthlyPayment);
 
-        int twoYearMonthlyPayment = service.calculate(1_000_000, 2);
+        int twoYearMonthlyPayment = service.calculate(1_000_000, 2, 9.99);
         System.out.println(twoYearMonthlyPayment);
 
-        int threeYearMonthlyPayment = service.calculate(1_000_000, 3);
+        int threeYearMonthlyPayment = service.calculate(1_000_000, 3, 9.99);
         System.out.println(threeYearMonthlyPayment);
     }
 }
